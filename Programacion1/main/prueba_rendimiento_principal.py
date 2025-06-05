42ad3d48a330a45b9d75be563dbbb707195feaf9
Agustín Sotelo <agustinemiliano22@gmail.com>
2025-06-05 13:31:27 -0300

    Estructuramos el codigo main para un futuro hacer los test de rendimiento

--- Programacion1/main/prueba_rendimiento_principal.py
+++ Programacion1/main/prueba_rendimiento_principal.py
@@ -1,12 +1,28 @@
-# Este es el archivo principal que ejecuta las pruebas de rendimiento.
+# Archivo "main" que ejecuta las pruebas
 
 # Importamos las librerías necesarias.
 import random         # Para generar números aleatorios de forma sencilla.
 import timeit         # Para medir el tiempo de ejecución.
 import sys            # Para ajustar el límite de recursión si es necesario.
 
-# Importamos nuestras funciones desde los otros archivos.
-# Asegúrate de que 'funciones_iterativas.py' y 'funciones_recursivas.py'
-# estén en la misma carpeta que este archivo.
+# Importamos nuestras funciones:
 from funciones_iterativas import calcular_promedio_positivos_iterativo
 from funciones_recursivas import calcular_promedio_positivos_recursivo
+
+# Pruebas - Medición 
+"""
+Ajustamos el límite de recursión de Python. Por defecto, Python detiene la recursión
+después de un cierto número de llamadas (normalmente 1000) para evitar que el programa
+se quede sin memoria. Lo aumentamos para poder probar con listas más grandes.
+Un número demasiado alto puede causar problemas de memoria 
+"""
+sys.setrecursionlimit(200000) 
+
+# Defimos los diferentes tamaños de lista para ejecutar nuestras pruebas, queremos ver
+# la diferencia de tiempos.
+tamanos_de_lista = [100, 1000, 5000, 10000, 50000, 100000]
+
+# inicializamos listas vacías para guardar los tiempos que medimos para cada función
+
+tiempos_funcion_iterativa = []
+tiempos_funcion_recursiva = []
