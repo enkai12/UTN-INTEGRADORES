605fa1dbcfa0d4121628b1f0f8fb9d275f4600e6
Agustín Sotelo <agustinemiliano22@gmail.com>
2025-06-05 11:43:06 -0300

    Se agregan pdfs a docs

---
+++ Programacion1/main/funciones_iterativas.py
@@ -0,0 +1,4 @@
+# Este archivo contiene la función para calcular el promedio de números positivos de forma iterativa.
+
+# --- Función Iterativa: 'calcular_promedio_positivos_iterativo' ---
+# Esta función encuentra el promedio de números positivos usando un bucle (paso a paso).
