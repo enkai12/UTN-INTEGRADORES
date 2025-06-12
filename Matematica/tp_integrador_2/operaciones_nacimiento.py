402c6491080ddc1561ffc27a851376747f7f65cc
Agustín Sotelo <agustinemiliano22@gmail.com>
2025-06-12 19:39:20 -0300

    Finalizo con los calculos de los conjuntos del ej.6

--- Matematica/tp_integrador_2/operaciones_nacimiento.py
+++ Matematica/tp_integrador_2/operaciones_nacimiento.py
@@ -98,114 +98,28 @@ else:
     print(f"El año {anio_a_verificar} No es Bisiesto.")
     
 
+# 6. Calcular el producto cartesiano entre el conjunto de años y el conjunto de edades actuales
+print("\n--Calculo de Producto Cartesiano--")
 
+anio_actual = datetime.datetime.now().year
+edad_1 = anio_actual - anio_nacimiento_1
+edad_2 = anio_actual - anio_nacimiento_2
 
+print(f"Edad calculada para el 1er integrante: {edad_1} años.")
+print(f"Edad calculada para el 2do integrante: {edad_2} años.")
 
+# convierto el año y edad en texto, y se pasa a una lista en digitos individuales
+digitos_anio_1 = list(str(anio_nacimiento_1))
+digitos_edad_1 = list(str(edad_1))
 
+digitos_anio_2 = list(str(anio_nacimiento_2))
+digitos_edad_2 = list(str(edad_2))
 
+# Calculamos y mostramos los productos cartesianos.
+producto_1 = calcular_producto_cartesiano(digitos_anio_1, digitos_edad_1)
+print(f"\nEl producto cartesiano de los digitos de {anio_nacimiento_1} y {edad_1} es:")
+print(producto_1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-nac_1 = int(input("Ingrese el primer año de nacimiento: "))
-nac_2 = int(input("Ingrese el segundo año de nacimiento: "))
-
-print(f"Año de Nacimiento N°1: {nac_1}")
-print(f"Año de Nacimiento N°2: {nac_2}")
-
-#   Contar cuántos nacieron en años pares e impares utilizando estructuras repetitivas.
-
-listaAños = []
-listaAños.append(nac_1)
-listaAños.append(nac_2)
-
-contadorPar = 0
-contadorImpar = 0 
-
-for i in listaAños:
-    if i % 2 == 0:
-        contadorPar += 1
-    else:
-        contadorImpar += 1
-
-print(f"La cantidad de años pares es: {contadorPar}. Y la cantidad de años impares es: {contadorImpar}")
-
-#   Si todos nacieron después del 2000, mostrar "Grupo Z".
-
-if nac_1 > 2000 and nac_2 > 2000:
-    print("Grupo Z")
-
-#   Si alguno nació en año bisiesto, mostrar "Tenemos un año especial".
-
-if ((nac_1 % 4 == 0 and nac_1 % 100 != 0) or (nac_1 % 400 == 0)) or ((nac_2 % 4 == 0 and nac_2 % 100 != 0) or (nac_2 % 400 == 0)):  # Se evalúa los dos años ingresados (or),
-    print("Tenemos un año especial")                                                                                                # al menos uno de ellos debe ser bisiesto
-
-#   Implementar una función para determinar si un año es bisiesto.
-
-def bisiesto(año):
-    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
-        return print("El año es bisiesto")
-    else:
-        return print("El año NO es bisiesto")
-
-año = int(input("Ingrese un año para determinar si es bisiesto: "))
-bisiesto(año)
-
-#   Calcular el producto cartesiano entre el conjunto de años y el conjunto de edades actuales.
-
-listaNac1 = []                  # Creamos listas vacias para incoporar cada digito del año como elemento
-listaNac2 = []
-for i in str(nac_1):            # Es necesario transformar el entero en cadena (str) para poder iterar
-    listaNac1.append(i)
-for i in str(nac_2):
-    listaNac2.append(i)
-
-añoActual = datetime.datetime.now().year  # Obtenemos el año actual para luego calcular la edad (sin considerar día/mes)
-edad1 = añoActual - nac_1
-edad2 = añoActual - nac_2
-
-listaEdad1 = []                  # Repetimos el proceso de armar una lista para la edad
-listaEdad2 = []                   
-for i in str(edad1):           
-    listaEdad1.append(i)
-for i in str(edad2):
-    listaEdad2.append(i)
-
-def productoCartesiano(conjuntoAño, conjuntoEdad):  # Creamos una función para armar el producto cartesiano con blucles
-    producto = []                                   # Definimos una variable como lista vacia
-    for a in conjuntoAño:                           # El bucle recorre la lista del año ingresada
-        for b in conjuntoEdad:                          # Y por cada iteración, el bucle anidado recorre la lista de la edad ingresada
-            producto.append((a, b))                         # En cada iteración se agrega a la lista 'producto' los valores determinados por a y b
-    return producto
-
-print(f"El producto cartesiando de {listaNac1} x {listaEdad1} es {productoCartesiano(listaNac1, listaEdad1)}")
-print(f"El producto cartesiando de {listaNac2} x {listaEdad2} es {productoCartesiano(listaNac2, listaEdad2)}")
+producto_2 = calcular_producto_cartesiano(digitos_anio_2, digitos_edad_2)
+print(f"\nEl producto cartesiano de los digitos de {anio_nacimiento_2} y {edad_2} es:")
+print(producto_2)
